28e13bd94ef48d758bf1f44d5ec50d3ebe308512
byron <byron@cwgsyw.com>
2026-06-11 14:40:45 +0800

    fix(user): add @JsonIgnore to password field to prevent BCrypt hash leak in JSON responses

--- backend/src/main/java/com/cwgsyw/platform/module/user/entity/User.java
+++ backend/src/main/java/com/cwgsyw/platform/module/user/entity/User.java
@@ -2,6 +2,7 @@ package com.cwgsyw.platform.module.user.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.cwgsyw.platform.common.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -11,6 +12,7 @@ import lombok.EqualsAndHashCode;
 public class User extends BaseEntity {
     private Long groupId;
     private String username;
+    @JsonIgnore
     private String password;
     private String realName;
     private String email;
